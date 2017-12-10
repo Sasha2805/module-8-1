f0d61a88897069c13ef4af2ce605b426a8e28ac6
Sasha Kaminska <sasha249133@ukr.net>
2017-12-10 23:20:27 +0200

    second task is done

--- src/main/java/SummationOfArrayElements.java
+++ src/main/java/SummationOfArrayElements.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
-public class CountingNumbersInArray {
+public class SummationOfArrayElements {
     private ArrayList<Integer> array;
-    int arraySize;
-    int countThreads;
+    private int arraySize;
+    private int countThreads;
 
-    public CountingNumbersInArray(int arraySize, int countThreads) {
+    public SummationOfArrayElements(int arraySize, int countThreads) {
         this.array = new ArrayList<>();
         this.arraySize = arraySize;
         this.countThreads = countThreads;
@@ -24,29 +28,6 @@ public class CountingNumbersInArray {
         return Math.sin(x) + Math.cos(x);
     }
 
-    // Метод расбивает массив на количество промежутков, равных количеству потоков.
-    // Каждый поток будет суммировать элементы в своем промежутке.
-    public ArrayList<FutureTask<Double>> distributionOfIntervals() {
-         if (countThreads > array.size()){
-            System.out.println("Количество потоков больше размера массива!");
-            return null;
-        }
-        int interval = array.size() / countThreads;
-        ArrayList<FutureTask<Double>> tasks = new ArrayList<>();
-        for (int i = 0; i < array.size();) {
-            int finalI = i;
-            if (i == ((countThreads * interval) - interval)){
-                tasks.add(new FutureTask<>(() -> sumElements(finalI, array.size())));
-                System.out.println(finalI+" "+array.size());
-                break;
-            }
-            tasks.add(new FutureTask<>(() -> sumElements(finalI, finalI + interval)));
-            System.out.println(finalI+" "+(finalI + interval));
-            i += interval;
-        }
-        return tasks;
-    }
-
     // Суммируем элементы в указанном промежутке массива
     public double sumElements(int first, int last){
         double sum = 0;
@@ -56,9 +37,95 @@ public class CountingNumbersInArray {
         return sum;
     }
 
-    public static void main(String[] args) {
-        CountingNumbersInArray counting = new CountingNumbersInArray(8_000_000, 3);
-        counting.arrayFill();
-        counting.distributionOfIntervals();
+    // Метод возвращает массив тасков, которые суммируют элементы в указанном промежутке.
+    // Количество промежутков равно количеству потоков.
+    public ArrayList<FutureTask<Double>> getIntervals() {
+        if (countThreads > array.size()){
+            System.out.println("Количество потоков больше размера массива!");
+            return null;
+        }
+        int interval = array.size() / countThreads;
+        ArrayList<FutureTask<Double>> tasks = new ArrayList<>();
+        for (int i = 0; i < array.size();) {
+            int finalI = i;
+            if (i == ((countThreads * interval) - interval)){
+                tasks.add(new FutureTask<>(() -> sumElements(finalI, array.size())));
+                break;
+            }
+            tasks.add(new FutureTask<>(() -> sumElements(finalI, finalI + interval)));
+            i += interval;
+        }
+        return tasks;
+    }
+
+    // Запуск подсчета
+    public void startCount(ArrayList<FutureTask<Double>> tasks) throws ExecutionException, InterruptedException {
+        for (FutureTask<Double> task : tasks) {
+            new Thread(task).start();
+        }
+        getSum(tasks);
+    }
+
+    // Повторение подсчетов
+    public void startExecutor(int n, ArrayList<FutureTask<Double>> tasks){
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < n; i++){
+            pool.submit(() -> {
+                try {
+                    startCount(tasks);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        pool.shutdown();
+    }
+
+    // Метод извлекает суммы из тасков и выводит общую сумму в консоль
+    public void getSum(ArrayList<FutureTask<Double>> tasks) throws ExecutionException, InterruptedException {
+        double sum = 0;
+        for (int i = 0; i < tasks.size(); i++){
+            while (!tasks.get(i).isDone()){
+                System.out.println("Ждем, пока FutureTask " + i + " не закончит свое выполнение");
+            }
+            sum += tasks.get(i).get();
+        }
+        System.out.println("Сумма элементов: " + sum);
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите количество элементов массива: ");
+        int size = in.nextInt();
+        System.out.print("Введите количество выполняемых потоков: ");
+        int countThreads = in.nextInt();
+        System.out.print("Введите количество подсчетов: ");
+        int n = in.nextInt();
+        in.close();
+
+        SummationOfArrayElements summation = new SummationOfArrayElements(size, countThreads);
+        summation.arrayFill();
+
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+        pool.submit(() -> {
+            long time = System.currentTimeMillis();
+            try {
+                summation.startCount(summation.getIntervals());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Executor отработал за " + (System.currentTimeMillis() - time) + " миллисекунд");
+        });
+
+        pool.submit(() -> {
+            long time = System.currentTimeMillis();
+            try {
+                summation.startExecutor(n, summation.getIntervals());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Executor отработал за " + (System.currentTimeMillis() - time) + " миллисекунд");
+        });
+        pool.shutdown();
     }
 }
