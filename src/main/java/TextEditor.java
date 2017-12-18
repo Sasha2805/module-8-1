2494666d3535eb4ee7ca82c35078bde1e6b4230c
Sasha Kaminska <sasha249133@ukr.net>
2017-12-18 17:17:12 +0200

    changed TextEditor

--- src/main/java/TextEditor.java
+++ src/main/java/TextEditor.java
@@ -1,11 +1,12 @@
-package FxApps;
-
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -41,27 +42,38 @@ public class TextEditor extends Application {
         textArea.setTranslateX(10);
         textArea.setTranslateY(inputPath.getTranslateY() + inputPath.getMinHeight() + 10);
 
+        // Окно для вывода ошибок
+        Label exceptionText = new Label();
+        VBox exceptionWindow = new VBox();
+        exceptionWindow.setAlignment(Pos.CENTER);
+        exceptionWindow.getChildren().add(exceptionText);
+
+        Stage exceptionStage = new Stage();
+        exceptionStage.setTitle("Error");
+        exceptionStage.setScene(new Scene(exceptionWindow, 200, 100));
+
+        // Загрузка файла по url
         Button loadFile = new Button("Load");
         loadFile.setOnAction(event -> {
-                new Thread(() -> {
-                    try {
-                        URL website = new URL(inputPath.getText());
-                        ReadableByteChannel channel = Channels.newChannel(website.openStream());
-                        FileOutputStream stream = new FileOutputStream(FILE_PATH);
-                        stream.getChannel().transferFrom(channel, 0, Long.MAX_VALUE);
-                        System.out.println("Загрузка завершилась успешно");
-                        String file = new Scanner(new File(FILE_PATH)).useDelimiter("\\Z").next();
-                        textArea.setText(file);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        System.out.println("Загрузка завершилась с ошибкой...");
-                    }
-                }).start();
+            new Thread(() -> {
+                try {
+                    URL website = new URL(inputPath.getText());
+                    ReadableByteChannel channel = Channels.newChannel(website.openStream());
+                    FileOutputStream stream = new FileOutputStream(FILE_PATH);
+                    stream.getChannel().transferFrom(channel, 0, Long.MAX_VALUE);
+                    String file = new Scanner(new File(FILE_PATH)).useDelimiter("\\Z").next();
+                    textArea.setText(file);
+                } catch (Exception e) {
+                    exceptionText.setText("Загрузка завершилась с ошибкой...");
+                    exceptionStage.show();
+                }
+            }).start();
         });
         loadFile.setTranslateX(inputPath.getTranslateX() + inputPath.getMinWidth() + 10);
         loadFile.setTranslateY(10);
         loadFile.setMaxWidth(100);
 
+        // Сохраняем в файл
         Button save = new Button("Save");
         save.setOnAction(event -> {
             new Thread(() -> {
@@ -72,7 +84,8 @@ public class TextEditor extends Application {
                     fileWriter.flush();
                     fileWriter.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    exceptionText.setText("Ошибка записи в файл!");
+                    exceptionStage.show();
                 }
             }).start();
         });
